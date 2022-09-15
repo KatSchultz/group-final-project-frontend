f8fb284e99dbfbc3103c3cddc4fd92fd319782a7
auriellove <auriel.e.love@gmail.com>
2022-09-14 21:43:24 -0400

    types and page setup

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,1 @@
+import { Palette } from "../types/palette.types";
---
+++ src/types/palette.types.ts
@@ -0,0 +1,7 @@
+export interface Palette {
+  primaryColor: string;
+  secondaryColor: string;
+  tertiaryColor: string;
+  name: string;
+  id: string;
+}
