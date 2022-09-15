a8ffa2ece84dbe25cc7cda8c67934e4053341aa2
Ian Velasquez <ianvelasquez62@yahoo.com>
2022-09-15 15:40:43 -0400

    still figuring out api calls, authentication and more files

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,1 +1,17 @@
 import { Palette } from "../types/palette.types";
+import { Color } from "../types/colors.types";
+import { getColor } from "../services/color.service";
+
+
+
+
+export default function DashboardPage() {
+  
+
+  console.log(getColor('hac123'));
+
+  return (
+    <div>DashboardPage</div>
+    )
+  }
+  
--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,6 +1,7 @@
 export interface User {
-  _id: string;
-  email: string;
-  displayName: string;
-  photoURL: string;
+  _id?: string;
+  uid?: string;
+  email?: string;
+  displayName?: string;
+  photoURL?: string;
 }
