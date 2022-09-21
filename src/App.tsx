394c88914dda2748e87d2513b7e5c514276c6fec
KatSchultz <ks.katieschultz@gmail.com>
2022-09-21 19:41:57 -0400

    made dashboard visible before login

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 import "./App.css";
 import DashboardPage from "./pages/DashboardPage";
-import SignupPage from './pages/SignupPage';
-import PrivateRoute from './components/PrivateRoute'
-import ProfilePage from './pages/ProfilePage';
+import SignupPage from "./pages/SignupPage";
+import PrivateRoute from "./components/PrivateRoute";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
     <div className="App w-full">
       <Routes>
-      <Route path='/' element={
-        <PrivateRoute>
-          <DashboardPage /> 
-        </PrivateRoute>}
-      />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/signup' element={<SignupPage />} />
-      <Route path='/:profileUsername' element={<ProfilePage />} />
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route
+          path="/:profileUsername"
+          element={
+            <PrivateRoute>
+              <ProfilePage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </div>
   );
